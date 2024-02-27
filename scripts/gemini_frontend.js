4c59f5a2d925794a6a6bf487bb963adbbff7824c
Vaibhav Rastogi <rastogi04vaibhav@gmail.com>
2024-02-27 17:08:13 +0530

    implemented 70% of chatbot frontend

--- scripts/gemini_frontend.js
+++ scripts/gemini_frontend.js
@@ -35,6 +35,31 @@ function geminiFrontEnd() {
   chatBotContentDiv.classList.add("chatbot-content");
   innerBody.append(chatBotContentDiv);
 
+  const chatBotMessageDiv = document.createElement("div");
+  chatBotMessageDiv.innerText =
+    "Hi there!  I'm Nix, the friendly chatbot assistant at PhotoNix, your one-stop shop for all things retro photography! How can I help you capture the moment in timeless style today?";
+  chatBotMessageDiv.classList.add("chatbot-message");
+  chatBotContentDiv.append(chatBotMessageDiv);
+
+  const messageDiv = document.createElement("div");
+  messageDiv.innerText = "What is Photonix?";
+  messageDiv.classList.add("user-message");
+  chatBotContentDiv.append(messageDiv);
+
+  const footerDiv = document.createElement("div");
+  footerDiv.classList.add("footer-div");
+  chatBotContentDiv.append(footerDiv);
+
+  const enterPrompt = document.createElement("input");
+  enterPrompt.setAttribute("placeholder", "Enter Prompt:");
+  enterPrompt.classList.add("prompt-div");
+  footerDiv.append(enterPrompt);
+
+  const sendPromptButton = document.createElement("button");
+  sendPromptButton.innerText = "Send";
+  //   sendPromptButton.classList.add("catalogue_button");
+  footerDiv.append(sendPromptButton);
+
   const body = document.getElementById("body");
   body.prepend(outerBody);
   outerBody.append(innerBody);
