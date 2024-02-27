8a73630a917d470c02af11e8655b30833500edd8
Vaibhav Rastogi <rastogi04vaibhav@gmail.com>
2024-02-27 16:31:03 +0530

    50% completion of implementing gemini

---
+++ scripts/gemini_frontend.js
@@ -0,0 +1,47 @@
+function geminiFrontEnd() {
+  const outerBody = document.createElement("div");
+  outerBody.classList.add("outer-body");
+
+  const innerBody = document.createElement("div");
+  innerBody.classList.add("inner-body");
+
+  const chatBotHeader = document.createElement("div");
+  chatBotHeader.classList.add("chatbot-header");
+  const chatBotText = document.createElement("div");
+  chatBotText.innerText = "Nix - ai";
+  chatBotText.classList.add("chatbot-header-text");
+  chatBotHeader.append(chatBotText);
+
+  const closeIcon = document.createElement("div");
+  closeIcon.innerHTML = `<a href="" onclick="removeGemini()"
+  ><svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="22"
+    height="22"
+    fill="red"
+    class="bi bi-x-square-fill"
+    viewBox="0 0 16 16"
+  >
+    <path
+      d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708"
+    /></svg
+></a>`;
+  chatBotHeader.classList.add("close-button");
+  chatBotHeader.append(closeIcon);
+
+  innerBody.append(chatBotHeader);
+
+  const chatBotContentDiv = document.createElement("div");
+  chatBotContentDiv.classList.add("chatbot-content");
+  innerBody.append(chatBotContentDiv);
+
+  const body = document.getElementById("body");
+  body.prepend(outerBody);
+  outerBody.append(innerBody);
+}
+
+function removeGemini() {
+  const outerBody = document.getElementsByClassName("outer-body");
+  const body = document.getElementById("body");
+  body.remove(outerBody);
+}
